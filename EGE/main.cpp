74fa6118393df1ce4112a75eef6fc54448f82570
Zajceva Darya <no@no.no>
2021-11-09 11:28:06 +0300

    show all pairs

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -6,8 +6,8 @@ using namespace std;
 void number_of_products_div()
 {
      /**
-     1. считать и запомнить числа +
-     2. вывести все пары чисел
+     +1. считать и запомнить числа
+     +2. вывести все пары чисел
      3. вместо пар вывести произведения
      4. проверить делимость произведений
      5. просчитать и вывести количество подходящих произведений
@@ -17,8 +17,12 @@ void number_of_products_div()
      vector <int> numbers(n,0);
      for (int i(0); i < n; i++)
         cin >> numbers[i];
-     for (int i(0); i < n; i++)
-        cout << numbers[i] << " ";
+
+     for (int j(0); j < n-1; j++)
+        for (int i(j+1); i < n; i++)
+            cout << numbers[j] << " " << numbers[i] << endl;
+
+
 
 }
 
