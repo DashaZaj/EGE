0999602e5581bf983263240e534326ab2e02d797
Zajceva Darya <no@no.no>
2021-11-09 12:01:18 +0300

    cin number

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -2,8 +2,20 @@
 
 using namespace std;
 
+void number_of_products_div()
+{
+/**
++1. считать число.
+2. добавить 1 в один из счётчиков
+3. вычислить и вывести ответ n33*n11 + n33*n3 + n33*no + n33*(n33-1)/2
+*/
+  int n(0);
+  cin >> n;
+
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    number_of_products_div();
     return 0;
 }
