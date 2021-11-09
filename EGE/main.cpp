7c74892d1f5250c37ad4fbccfca129936aba527a
Zajceva Darya <no@no.no>
2021-11-09 11:00:22 +0300

    read and show numbers

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 void number_of_products_div()
 {
-     cout << "Hello world!" << endl;
      /**
-     1. считать и запомнить числа
+     1. считать и запомнить числа +
      2. вывести все пары чисел
      3. вместо пар вывести произведения
      4. проверить делимость произведений
      5. просчитать и вывести количество подходящих произведений
      */
+     int n(0);
+     cin >> n;
+     vector <int> numbers(n,0);
+     for (int i(0); i < n; i++)
+        cin >> numbers[i];
+     for (int i(0); i < n; i++)
+        cout << numbers[i] << " ";
+
 }
 
 int main()
