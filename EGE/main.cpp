7af2381ae33ef09d559745072e2f53be45a64a42
Zajceva Darya <no@no.no>
2021-11-16 10:46:12 +0300

    all numbers cin(ed) and cout(ed)

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 void max_sum_div()
 {
     /**
-    1. считать и сохранить все числа
+    +1. считать и сохранить все числа
     2. вывести все пары
     3. вместо пар вывести суммы
     4. проверить делимость сумм
     5. вывести только наибольшую сумму
     */
-    cout << "Hello world!" << endl;
+    int n(0);
+    cin >> n;
+    vector <int> numbers(n, 0);
+    for (int i(0); i < n; i++)
+        cin >> numbers[i];
+    cout << endl;
+    for (int i(0); i < n; i++)
+        cout << numbers[i] << " ";
 }
 
 int main()
