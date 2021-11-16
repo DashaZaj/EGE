a5c16e409da5983d91fb3d2a0dcbe72621148a82
Zajceva Darya <no@no.no>
2021-11-16 10:40:01 +0300

    added development plan

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -2,8 +2,20 @@
 
 using namespace std;
 
+void max_sum_div()
+{
+    /**
+    1. считать и сохранить все числа
+    2. вывести все пары
+    3. вместо пар вывести суммы
+    4. проверить делимость сумм
+    5. вывести только наибольшую сумму
+    */
+    cout << "Hello world!" << endl;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    max_sum_div();
     return 0;
 }
