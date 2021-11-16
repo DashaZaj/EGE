59d9827d7b9b01b9d763eb829176b11b7bff8552
Zajceva Darya <no@no.no>
2021-11-16 10:52:03 +0300

    pairs are displayed

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -7,7 +7,7 @@ void max_sum_div()
 {
     /**
     +1. считать и сохранить все числа
-    2. вывести все пары
+    +2. вывести все пары
     3. вместо пар вывести суммы
     4. проверить делимость сумм
     5. вывести только наибольшую сумму
@@ -18,8 +18,9 @@ void max_sum_div()
     for (int i(0); i < n; i++)
         cin >> numbers[i];
     cout << endl;
-    for (int i(0); i < n; i++)
-        cout << numbers[i] << " ";
+    for (int i(0); i < n-1; i++)
+        for (int j(i+1); j<n; j++)
+           cout << numbers[i] << " " << numbers[j] << endl;
 }
 
 int main()
