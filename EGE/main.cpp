9ea3a35bf3f9325b70c423c366d5d3113dbeadc8
Zajceva Darya <no@no.no>
2021-11-16 11:53:57 +0300

    max_sum is shown

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -12,22 +12,22 @@ void max_sum_div()
     +2. вывести все пары
     +3. вместо пар вывести суммы
     +4. проверить делимость сумм
-    5. вывести только наибольшую сумму
+    +5. вывести только наибольшую сумму
     */
     int n(0);
     ifs >> n;
     vector <int> numbers(n, 0);
     for (int i(0); i < n; i++)
         ifs >> numbers[i];
-    cout << endl;
+    int max_sum(0);
     for (int i(0); i < n-1; i++){
         for (int j(i+1); j<n; j++){
            int sum(numbers[i] + numbers[j]);
-           if (sum%2 == 1)
-                cout << sum << endl;
+           if (sum%2 == 1 and sum > max_sum)
+                max_sum = sum;
         }
     }
-
+    cout << max_sum << endl;
 }
 
 int main()
