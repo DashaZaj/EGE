02dd2253f771beddfce01e717707018370bc936e
Zajceva Darya <no@no.no>
2021-11-16 11:24:19 +0300

    reads from file, shows odd sums

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -1,26 +1,33 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
 void max_sum_div()
 {
+    ifstream ifs("data.txt");
     /**
     +1. считать и сохранить все числа
     +2. вывести все пары
     +3. вместо пар вывести суммы
-    4. проверить делимость сумм
+    +4. проверить делимость сумм
     5. вывести только наибольшую сумму
     */
     int n(0);
-    cin >> n;
+    ifs >> n;
     vector <int> numbers(n, 0);
     for (int i(0); i < n; i++)
-        cin >> numbers[i];
+        ifs >> numbers[i];
     cout << endl;
-    for (int i(0); i < n-1; i++)
-        for (int j(i+1); j<n; j++)
-           cout << numbers[i] + numbers[j] << endl;
+    for (int i(0); i < n-1; i++){
+        for (int j(i+1); j<n; j++){
+           int sum(numbers[i] + numbers[j]);
+           if (sum%2 == 1)
+                cout << sum << endl;
+        }
+    }
+
 }
 
 int main()
