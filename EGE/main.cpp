5b2367187b966859785824bac857c290d608efaf
Dasha_Zajc <no@no.no>
2021-11-22 22:49:19 +0300

    max_sum can be found

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -8,8 +8,8 @@ void max_sum_div(){
     /**
     +1. ввести числа
     +2. найти максимальные числа, дающие остаток 1, 2, 3, 4, 5 или 6 при делении на 7 и два, кратных семи
-    3. проверить, кратно ли m1*m6, m2*m5, m3*m4 или m01*m02 трём и больше ли каждый из них -1
-    4. если да, то вывести сумму, нет - вывести FAIL
+    3. проверить, кратно ли m1*m6, m2*m5, m3*m4 или m01*m02 трём и больше ли каждый из них -1 и найти большую сумму
+    4. если нашлт, то вывести сумму, нет - вывести FAIL
     */
     int n(0), cur(0), m1(-1), m2(-1), m3(-1), m4(-1), m5(-1), m6(-1), m01(-1), m02(-1);
     ifs >> n;
@@ -24,10 +24,12 @@ void max_sum_div(){
         else if (cur%7 == 5 and cur > m5) m5 = cur;
         else if (cur%7 == 6 and cur > m6) m6 = cur;
     }
-    cout << m01 << " " << m02 << endl;
-    cout << m1 << " " << m6 << endl;
-    cout << m2 << " " << m5 << endl;
-    cout << m3 << " " << m4 << endl;
+    int max_sum(0);
+    if (m01 > -1 and m02 > -1 and (m01*m02)%3 ==0 and m01+m02 > max_sum) max_sum = m01 + m02;
+    else if (m1 > -1 and m6 > -1 and (m1*m6)%3 ==0 and m1+m6 > max_sum) max_sum = m1 + m6;
+    else if (m2 > -1 and m5 > -1 and (m2*m5)%3 ==0 and m2+m5 > max_sum) max_sum = m2 + m5;
+    else if (m3 > -1 and m4 > -1 and (m3*m4)%3 ==0 and m3+m4 > max_sum) max_sum = m3 + m4;
+    cout << max_sum;
 }
 
 int main()
