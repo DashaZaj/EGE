b22340fccc441cfcfabc6b320fd7d0b2b7e56193
Dasha_Zajc <no@no.no>
2021-11-22 21:46:27 +0300

    added action when not found, starting values of 'max_' variables are changed

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -9,16 +9,17 @@ void max_sum_div()
     /**
     +1. ввести количество цифр в сообщении
     +2. найти максимальное чётное и максимальное нечётное число
-    3. если нашли - складываем и выводим, не нашли - выводим -1
+    +3. если нашли - складываем и выводим, не нашли - выводим -1
     */
-    int n(0), cur(0), max_odd(0), max_even(0);
+    int n(0), cur(0), max_odd(-1001), max_even(-1001);
     ifs >> n;
     for (int i(0); i < n; i++){
         ifs >> cur;
         if (cur % 2 == 0 and cur > max_even) max_even = cur;
         else if (cur > max_odd) max_odd = cur;
     }
-    cout << max_even << " " << max_odd;
+    if (max_even != -1001 and max_odd != -1001) cout << max_even + max_odd << endl;
+    else cout << -1 << endl;
 }
 
 int main()
