dd019fc9e8e1802793e848887e80438e61c57dea
Dasha_Zajc <no@no.no>
2021-11-22 21:34:51 +0300

    reads number of digits from file

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 void max_sum_div()
 {
+    ifstream ifs("data.txt");
     /**
-    1. ввести количество цифр в сообщении
+    +1. ввести количество цифр в сообщении
     2. найти максимальное чётное и максимальное нечётное число
     3. если нашли - складываем и выводим, не нашли - выводим -1
     */
-    cout << "Hellow World" << endl;
+    int n(0);
+    ifs >> n;
+    cout << n;
 }
 
 int main()
