bed71538ffea9579a14ede01aacc094a5d89b7fc
Dasha_Zajc <no@no.no>
2021-11-22 22:04:03 +0300

    the condition and action when not found are changed to match the task

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -23,12 +23,13 @@ void max_sum_div()
     for (int i(0); i < n-1; i++){
         for (int j(i+1); j<n; j++){
            int sum(numbers[i] + numbers[j]);
-           if (sum%2 == 1 and sum > max_sum)
+           int product(numbers[i]*numbers[j]);
+           if (sum%7 == 0 and product%3 ==0 and sum > max_sum)
                 max_sum = sum;
         }
     }
     if (max_sum != 0) cout << max_sum << endl;
-    else cout << -1 << endl;
+    else cout << "FAIL" << endl;
 }
 
 int main()
