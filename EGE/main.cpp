5dc862856db23f0df0bbde6594b6c4df99f2fb28
Dasha_Zajc <no@no.no>
2021-11-22 22:29:26 +0300

    can read numbers

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -6,12 +6,17 @@ using namespace std;
 void max_sum_div(){
     ifstream ifs("data_max_sum_div.txt");
     /**
-    1. ввести числа
+    +1. ввести числа
     2. найти максимальные числа, дающие остаток 1, 2, 3, 4, 5 или 6 при делении на 7 и два, кратных семи
     3. проверить, кратно ли m1*m6, m2*m5, m3*m4 или m01*m02 трём и больше ли их сумма -1
     4. если да, то вывести сумму, нет - вывести FAIL
     */
-    cout << "Hello world!" << endl;
+    int n(0), cur(0);
+    ifs >> n;
+    for (int i(0); i < n; i++){
+        ifs >> cur;
+        cout << cur << endl;
+    }
 }
 
 int main()
