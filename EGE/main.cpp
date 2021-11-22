6b452763db4bdd3045cba3fbcf38e587ca0eac7c
Dasha_Zajc <no@no.no>
2021-11-22 21:30:37 +0300

    added development plan

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -2,8 +2,18 @@
 
 using namespace std;
 
+void max_sum_div()
+{
+    /**
+    1. ввести количество цифр в сообщении
+    2. найти максимальное чётное и максимальное нечётное число
+    3. если нашли - складываем и выводим, не нашли - выводим -1
+    */
+    cout << "Hellow World" << endl;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    max_sum_div();
     return 0;
 }
