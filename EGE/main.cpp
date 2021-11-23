9e9c8af7d10608a1879c15023f64f3eb859e3f3d
Zajceva Darya <no@no.no>
2021-11-23 11:44:10 +0300

    numbers read and vector changed

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -9,14 +9,16 @@ void number_of_sums_div()
     ifstream ifs("number_of_sums_div_data.txt");
     /**
     +1. vector cчётчиков по остаткам от деления на 40
-    2. считываем числа и считаем, сколько есть чивел с разными остатками от деления на 40
+    +2. считываем числа и считаем, сколько есть чисел с разными остатками от деления на 40
     3. собираем пары
     */
     vector<int> counters(40,0);
 
     int n(0);
-    ifs >> n;
-    counters[n%40] ++;
+    while (n != -9999){
+        ifs >> n;
+        counters[n%40] ++;
+    }
 
     for (int i(0); i < counters.size(); i++)
         cout << i << ":" << counters[i] << endl;
