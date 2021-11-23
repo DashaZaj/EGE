b7fe9d378262e2725bc016bb87f200153bc72eb7
Zajceva Darya <no@no.no>
2021-11-23 12:03:34 +0300

    not ready, another bunch of pairs have to be created

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -12,16 +12,22 @@ void number_of_sums_div()
     +2. считываем числа и считаем, сколько есть чисел с разными остатками от деления на 40
     3. собираем пары
     */
-    vector<int> counters(40,0);
+    vector<int> counters_sm_or_eqw40(40,0);
+    vector<int> counters_big40(40,0);
 
     int n(0);
     while (n != -9999){
         ifs >> n;
-        counters[n%40] ++;
+        if (n <= 40) counters_sm_or_eqw40[n%40] ++;
+        if (n > 40) counters_big40[n%40]++;
     }
 
-    for (int i(0); i < counters.size(); i++)
-        cout << i << ":" << counters[i] << endl;
+    int sum(0);
+    for (int i(1); i < 19; i++)
+        sum += counters_big40[i] * counters_big40[40-i];
+    sum += counters_big40[0]*(counters_big40[0]-1)/2;
+    sum += counters_big40[20]*(counters_big40[20]-1)/2;
+    cout << sum << endl;
 }
 
 int main()
