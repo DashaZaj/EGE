6d45aa68fdf420efc49ce79af5b81cbc5264f38a
Zajceva Darya <no@no.no>
2021-11-23 11:27:23 +0300

    added development plan, can read from file

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
+void number_of_sums_div()
+{
+    /**
+    1. vector щётчиков по остаткам от деления на 40
+    2. считываем числа и считаем, сколько есть чивел с разными остатками от деления на 40
+    3. собираем пары
+    */
+    cout << "Hello world" << endl;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    number_of_sums_div();
     return 0;
 }
