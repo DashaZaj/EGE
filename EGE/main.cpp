55d0c497942bffebff2a3c27753094aad8093009
Dasha_Zajc <no@no.no>
2021-11-29 19:25:45 +0300

    all pair are found and counted

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -10,7 +10,7 @@ void number_of_sums_div()
     /**
     +1. vector cчётчиков по остаткам от деления на 40
     +2. считываем числа и считаем, сколько есть чисел с разными остатками от деления на 40
-    3. собираем пары
+    +3. собираем пары
     */
     vector<int> counters_sm_or_eqw40(40,0);
     vector<int> counters_big40(40,0);
@@ -27,6 +27,10 @@ void number_of_sums_div()
         sum += counters_big40[i] * counters_big40[40-i];
     sum += counters_big40[0]*(counters_big40[0]-1)/2;
     sum += counters_big40[20]*(counters_big40[20]-1)/2;
+    for (int i(1); i < 40; i++){
+        sum += counters_big40[i]*counters_sm_or_eqw40[40-i];
+    sum += counters_sm_or_eqw40[0]*counters_big40[0];
+    }
     cout << sum << endl;
 }
 
