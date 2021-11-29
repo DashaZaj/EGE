5c86d3ba281fd07d19dbc18ebe6ab937647d9528
Dasha_Zajc <no@no.no>
2021-11-29 21:26:53 +0300

    pairs are built and shown

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -9,7 +9,7 @@ void min_sum_not_farther_than_4()
     ifstream ifs("data_min_sum.txt");
     /**
     +1. ввести и сохранить чила
-    2. составить и вывести пары, подход€щие под условие
+    +2. составить и вывести пары, подход€щие под условие
     3. нийти и вывести минимум
     */
     int n(0);
@@ -18,8 +18,12 @@ void min_sum_not_farther_than_4()
     for (int i(0); i < n; i++)
         ifs >> numbers[i];
 
-    for (int i(0); i < n; i++)
-        cout << numbers[i] << " ";
+    for (int i(0); i < n-1; i++){
+        for (int j(i+1); j < n; j++){
+            if ( abs(j - i) >= 4)
+                cout << numbers[i] << " " << numbers[j] << endl;
+        }
+    }
 }
 
 int main()
