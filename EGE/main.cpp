13c1e4bbea743b2c2f771848e917155c218c6103
Dasha_Zajc <no@no.no>
2021-11-29 21:02:46 +0300

    can read from file; added development plan

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+void min_sum_not_farther_than_4()
+{
+    ifstream ifs("data_min_sum.txt");
+    /**
+    1. ввести и сохранить чила
+    2. составить и вывести пары, подход€щие под условие
+    3. нийти и вывести минимум
+    */
+    cout << "Hello world!" << endl;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    min_sum_not_farther_than_4();
     return 0;
 }
