b0880726cb45939c298dec954ed90b65c0e414a0
Dasha_Zajc <no@no.no>
2021-12-07 10:47:16 +0300

    "worm" has found min sum

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -17,28 +17,30 @@ void min_sum_distance()
     +2. число в хвосте - потенциальный минимум из пройденных
     +3. сдвинуть червяка влево
     +4. считать пятое число
-    !!5. вывести червяка и пройденный минимум для проверки
-    6. собрать пару из того, что в зубах червяка, и пройденного минимума
-    7. это кандидат на минимальную пару
-    8. повторяем
+    +!!5. вывести червяка и пройденный минимум для проверки
+    +6. собрать пару из того, что в зубах червяка, и пройденного минимума
+    +7. это кандидат на минимальную пару
+    +8. повторяем
     */
-    int n(0);
+    int min1(2001), n(0), par(2001);
     ifs >> n;
     vector <int> worm(4, 0);
     for (int i(0); i<4; i++){
         ifs >> worm[i];
     }
-    int min1=worm[0];
-    int temp(0);
-    for (int i(3); i>=0; i=i-1){
-        temp = temp + worm[i];
-        worm[i] = temp - worm[i];
-        temp = temp - worm[i];
+
+    for (int j(0); j < n-4; j++){
+        if (min1 > worm[0]) min1 = worm[0];
+        int temp(0);
+        for (int i(3); i>=0; i=i-1){
+            temp = temp + worm[i];
+            worm[i] = temp - worm[i];
+            temp = temp - worm[i];
+        }
+        ifs >> worm[3];
+        if (par > (min1 + worm[3])) par = min1 + worm[3];
     }
-    ifs >> worm[3];
-    for (int i(0); i<4; i++){
-        cout << worm[i] << " ";
-    }
+    cout << par << endl;
 }
 
 int main()
