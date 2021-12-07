caf73075673a5b76cbac92291d0a237bdb63899e
Dasha_Zajc <no@no.no>
2021-12-07 11:31:06 +0300

    can read from file; added development plan

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+void sum_div_30_not_farther_then_7(){
+    ifstream ifs("data.txt");
+    /**
+    1. считать числа в вектор
+    2. вывести все пары на расстоянии меньше, чем семь
+    3. вывести пары, кратные 30
+    4. вывести количество пар
+    */
+    cout << "Hello world!" << endl;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    sum_div_30_not_farther_then_7();
     return 0;
 }
