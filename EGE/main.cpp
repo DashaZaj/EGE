d03c74fa1a08283ea6869a2630f9dd722faa0fa0
Dasha_Zajc <no@no.no>
2021-12-07 11:37:22 +0300

    numbers are read

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <fstream>
 
 using namespace std;
@@ -6,12 +7,18 @@ using namespace std;
 void sum_div_30_not_farther_then_7(){
     ifstream ifs("data.txt");
     /**
-    1. считать числа в вектор
+    +1. считать числа в вектор
     2. вывести все пары на расстоянии меньше, чем семь
     3. вывести пары, кратные 30
     4. вывести количество пар
     */
-    cout << "Hello world!" << endl;
+    int n(0);
+    ifs >> n;
+    vector <int> numbers(n, 0);
+    for (int i(0); i < n; i++)
+        ifs >> numbers[i];
+    for (int i(0); i < n; i++)
+        cout << numbers[i] << " ";
 }
 
 int main()
