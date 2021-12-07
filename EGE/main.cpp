d8c97393ed46f7d3094d359db9fd5e8fc2133a36
Dasha_Zajc <no@no.no>
2021-12-07 11:48:42 +0300

    INEFF solution is found

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -10,19 +10,21 @@ void sum_div_30_not_farther_then_7(){
     +1. считать числа в вектор
     +2. вывести все пары на расстоянии меньше, чем семь
     +3. вывести пары, кратные 30
-    4. вывести количество пар
+    +4. вывести количество пар
     */
     int n(0);
     ifs >> n;
     vector <int> numbers(n, 0);
     for (int i(0); i < n; i++)
         ifs >> numbers[i];
+    int counter(0);
     for (int i(0); i < n; i++){
         for (int j(i+1); j< i+8 and j<n; j++){
             int par = numbers[i] + numbers[j];
-            if (par % 30 == 0) cout << par << endl;
+            if (par % 30 == 0) counter ++;
         }
     }
+    cout << counter << endl;
 }
 
 int main()
