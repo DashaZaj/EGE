22a7b9a0ef75e481793405a0205d63e0400406f2
Dasha_Zajc <no@no.no>
2021-12-14 14:17:03 +0300

    maxs are found

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -7,11 +7,23 @@ using namespace std;
 void max_sum_multiple_distance(){
     ifstream ifs("data.txt");
     /*
-    1. считать количество чисел, найти два максимума в каждом срезе с нужной кратностью
+    +1. считать количество чисел, найти два максимума в каждом срезе с нужной кратностью
     2. собрать суммы из максимумов каждого среза, найти и вывести максимум
     **/
     array<array<int, 2>, 6> maxs;
-    cout << "Hello world!" << endl;
+    for(int i(0); i < 6; i++)
+        for (int j(0); j<2; j++)
+            maxs[i][j] = 0;
+    int N(0), a(0);
+    ifs >> N;
+    for (int i(0); i < N; i++){
+        ifs >> a;
+        if (a > maxs[i%6][0]) maxs[i%6][0] = a;
+        else if (a > maxs[i%6][1]) maxs[i%6][1] = a;
+    }
+    for (int i(0); i < 6; i++){
+        cout << maxs[i][0] << " " << maxs[i][1] << endl;
+    }
 }
 
 int main()
