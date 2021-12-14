cf6580921ff44923c1c54c0a7b19451834c6305c
Dasha_Zajc <no@no.no>
2021-12-14 12:44:43 +0300

    can read from file; added development plan

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+void number_of_sums_div_30_not_farther_then_7(){
+    ifstream ifs("data.txt");
+    /*
+    1. считать 6 чисел в цикличный буфер
+    2. проверить все пары на кратность 30
+    3. считать ещё одно число
+    4. проверит 5 новых пар
+    5. повторять 3 и 4 пункты
+    6. вспомнить, что N может быть меньше 6
+    **/
+    cout << "Hello word!" << endl;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    number_of_sums_div_30_not_farther_then_7();
     return 0;
 }
