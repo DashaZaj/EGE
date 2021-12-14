b53f80586733f3b88b123675f340eef87e446e95
Dasha_Zajc <no@no.no>
2021-12-14 13:13:11 +0300

    "snake" moved

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -9,7 +9,7 @@ void number_of_sums_div_30_not_farther_then_7(){
     /*
     +1. считать 6 чисел в цикличный буфер
     +2. проверить все пары на кратность 30
-    3. считать ещё одно число
+    +3. считать ещё одно число
     4. проверит 5 новых пар
     5. повторять 3 и 4 пункты
     6. вспомнить, что N может быть меньше 6
@@ -17,13 +17,23 @@ void number_of_sums_div_30_not_farther_then_7(){
     int N(0);
     ifs >> N;
     vector <int> snake(6);
-    int head(snake[0]), tail(snake[1]), counter(0);
+    int head(5), tail(0), counter(0);
     for (int i(0); i < 6; i++)
         ifs >> snake[i];
     for (int j(0); j<5; j++)
         for (int l(j); l < 6; l++)
             if ((snake[j] + snake[l]) % 30 == 0) counter++;
+    for (int i(0); i < 6; i++)
+        cout << snake[i] << " ";
+    if (head <5) head++;
+    else head = 0;
+    if (tail < 5) tail++;
+    else tail = 0;
+    ifs >> snake[head];
+
     cout << counter << endl;
+    for (int i(0); i < 6; i++)
+        cout << snake[i] << " ";
 }
 
 int main()
