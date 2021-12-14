9bf466fdeb1691905afa68ed7513154504724451
Dasha_Zajc <no@no.no>
2021-12-14 13:59:26 +0300

    can read from file; added development plan

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <array>
+#include <fstream>
 
 using namespace std;
 
+void max_sum_multiple_distance(){
+    ifstream ifs("data.txt");
+    /*
+    1. считать количество чисел, найти два максимума в каждом срезе с нужной кратностью
+    2. собрать суммы из максимумов каждого среза, найти и вывести максимум
+    **/
+    array<array<int, 2>, 6> maxs;
+    cout << "Hello world!" << endl;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    max_sum_multiple_distance();
     return 0;
 }
