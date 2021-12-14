f0771e11a1f2acdabd9d01bd8d80ad063c9a55fd
Dasha_Zajc <no@no.no>
2021-12-14 13:33:48 +0300

    added condition if "snake" can't be created

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -12,27 +12,38 @@ void number_of_sums_div_30_not_farther_then_7(){
     +3. считать ещё одно число
     +4. проверит 5 новых пар
     +5. повторять 3 и 4 пункты
-    6. вспомнить, что N может быть меньше 6
+    +6. вспомнить, что N может быть меньше 6
     **/
     int N(0);
+    int counter(0);
     ifs >> N;
-    vector <int> snake(6);
-    int head(5), tail(0), counter(0);
-    for (int i(0); i < 6; i++)
-        ifs >> snake[i];
-    for (int j(0); j<5; j++)
-        for (int l(j); l < 6; l++)
-            if ((snake[j] + snake[l]) % 30 == 0) counter++;
-    for (int(y); y < N-6; y++){
-        if (head <5) head++;
-        else head = 0;
-        if (tail < 5) tail++;
-        else tail = 0;
-        ifs >> snake[head];
-        for (int i(0); i<6; i++){
-            if (i != head)
-                if ((snake[head] + snake[i]) % 30 == 0) counter++;
+    if (N>= 6){
+        vector <int> snake(6);
+        int head(5), tail(0);
+        for (int i(0); i < 6; i++)
+            ifs >> snake[i];
+        for (int j(0); j<5; j++)
+            for (int l(j); l < 6; l++)
+                if ((snake[j] + snake[l]) % 30 == 0) counter++;
+        for (int y(0); y < N-6; y++){
+            if (head <5) head++;
+            else head = 0;
+            if (tail < 5) tail++;
+            else tail = 0;
+            ifs >> snake[head];
+            for (int i(0); i<6; i++){
+                if (i != head)
+                    if ((snake[head] + snake[i]) % 30 == 0) counter++;
+            }
         }
+    }
+    else{
+        vector <int> numbers(N);
+        for (int i(0); i < N; i++)
+            ifs >> numbers[i];
+        for (int j(0); j<N-1; j++)
+            for (int l(j); l < N; l++)
+                if ((numbers[j] + numbers[l]) % 30 == 0) counter++;
     }
     cout << counter << endl;
 }
