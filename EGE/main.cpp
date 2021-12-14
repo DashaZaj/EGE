d54635278c5df1b27725f5fa6d35dbc4d30e0394
Dasha_Zajc <no@no.no>
2021-12-14 12:56:15 +0300

    "snake" created

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
 void number_of_sums_div_30_not_farther_then_7(){
     ifstream ifs("data.txt");
     /*
-    1. считать 6 чисел в цикличный буфер
+    +1. считать 6 чисел в цикличный буфер
     2. проверить все пары на кратность 30
     3. считать ещё одно число
     4. проверит 5 новых пар
     5. повторять 3 и 4 пункты
     6. вспомнить, что N может быть меньше 6
     **/
-    cout << "Hello word!" << endl;
+    int N(0);
+    ifs >> N;
+    vector <int> snake(6);
+    int head(snake[0]), tail(snake[1]);
+    for (int i(0); i < 6; i++)
+        ifs >> snake[i];
+    for (int i(0); i < 6; i++)
+        cout << snake[i] << " ";
 }
 
 int main()
