80d813e0c458b8119a00783c86ed324f85a3bc09
Dasha_Zajc <no@no.no>
2021-12-14 13:20:16 +0300

    loop created

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -11,7 +11,7 @@ void number_of_sums_div_30_not_farther_then_7(){
     +2. проверить все пары на кратность 30
     +3. считать ещё одно число
     +4. проверит 5 новых пар
-    5. повторять 3 и 4 пункты
+    +5. повторять 3 и 4 пункты
     6. вспомнить, что N может быть меньше 6
     **/
     int N(0);
@@ -23,14 +23,16 @@ void number_of_sums_div_30_not_farther_then_7(){
     for (int j(0); j<5; j++)
         for (int l(j); l < 6; l++)
             if ((snake[j] + snake[l]) % 30 == 0) counter++;
-    if (head <5) head++;
-    else head = 0;
-    if (tail < 5) tail++;
-    else tail = 0;
-    ifs >> snake[head];
-    for (int i(0); i<6; i++){
-        if (i != head)
-            if ((snake[head] + snake[i]) % 30 == 0) counter++;
+    for (int(y); y < N-6; y++){
+        if (head <5) head++;
+        else head = 0;
+        if (tail < 5) tail++;
+        else tail = 0;
+        ifs >> snake[head];
+        for (int i(0); i<6; i++){
+            if (i != head)
+                if ((snake[head] + snake[i]) % 30 == 0) counter++;
+        }
     }
     cout << counter << endl;
 }
