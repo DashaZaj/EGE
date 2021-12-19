843dbf961d49b573d37bd9ac37d678993fba0b43
Dasha_Zajc <no@no.no>
2021-12-19 18:04:52 +0300

    a mistake in calculation of distance fixed; can't read from file

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
 
 using namespace std;
 
 void number_of_sums_div_30_not_farther_then_7(){
-    ifstream ifs("data.txt");
     /*
     +1. считать 6 чисел в цикличный буфер
     +2. проверить все пары на кратность 30
@@ -16,33 +14,34 @@ void number_of_sums_div_30_not_farther_then_7(){
     **/
     int N(0);
     int counter(0);
-    ifs >> N;
-    if (N>= 6){
-        vector <int> snake(6);
-        int head(5), tail(0);
-        for (int i(0); i < 6; i++)
-            ifs >> snake[i];
-        for (int j(0); j<5; j++)
-            for (int l(j); l < 6; l++)
+    cin >> N;
+    if (N>= 7){
+        vector <int> snake(7);
+        int head(6), tail(0);
+        for (int i(0); i < 7; i++)
+            cin >> snake[i];
+        for (int j(0); j<6; j++)
+            for (int l(j+1); l < 7; l++)
                 if ((snake[j] + snake[l]) % 30 == 0) counter++;
-        for (int y(0); y < N-6; y++){
-            if (head <5) head++;
+        for (int y(0); y < N-7; y++){
+            if (head <6) head++;
             else head = 0;
-            if (tail < 5) tail++;
+            if (tail < 6) tail++;
             else tail = 0;
-            ifs >> snake[head];
-            for (int i(0); i<6; i++){
-                if (i != head)
+            cin >> snake[head];
+            for (int i(0); i<7; i++){
+                if (i != head){
                     if ((snake[head] + snake[i]) % 30 == 0) counter++;
+                }
             }
         }
     }
     else{
         vector <int> numbers(N);
         for (int i(0); i < N; i++)
-            ifs >> numbers[i];
+            cin >> numbers[i];
         for (int j(0); j<N-1; j++)
-            for (int l(j); l < N; l++)
+            for (int l(j+1); l < N; l++)
                 if ((numbers[j] + numbers[l]) % 30 == 0) counter++;
     }
     cout << counter << endl;
