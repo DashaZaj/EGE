eb80f7fe378c349fe51411a8b6fd23dce806b452
Dasha_Zajc <no@no.no>
2021-12-19 19:01:54 +0300

    naive solution is found

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -8,17 +8,20 @@ void num_sum_div_67_distance_not_closer_than_5(){
     ifstream ifs("data.txt");
     /*
     +1. записываем числа в массив
-    2. проверяем и считаем все возможные с учётом условия пары на кратность 67-ми
+    +2. проверяем и считаем все возможные с учётом условия пары на кратность 67-ми
     **/
-    int N(0);
+    int N(0), counter(0);
     ifs >> N;
     vector <int> numbers(N);
     for (int i(0); i < N; i++){
         ifs >> numbers[i];
     }
-    for (int i(0); i < N; i++){
-        cout << numbers[i] << " ";
+    for (int i(0); i < N-5; i++){
+        for (int j(i+5); j<N; j++){
+            if ((numbers[i] + numbers[j])%67 == 0) counter++;
+        }
     }
+    cout << counter << endl;
 }
 
 
