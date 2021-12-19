21521fab6f7d032cbc2efad0b0422102b64ffc4e
Dasha_Zajc <no@no.no>
2021-12-19 20:10:05 +0300

    array of vectors was created and displayed

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <array>
 #include <fstream>
 
 using namespace std;
@@ -7,13 +8,27 @@ using namespace std;
 void num_sum_div_67_distance_not_closer_than_5(){
     ifstream cin("data.txt");
     /*
-    1. создать массив векторв, содержащих номера чисел, соответственно остатку от деления на 67
+    +1. создать массив векторв, содержащих номера чисел, соответственно остатку от деления на 67
     2. найти пары векторов, остатки которых комплиминтарны
     3. для каждой пары остатков найти количество пар, отвечающих условию о расстоянии
     **/
     int N(0);
     cin >> N;
-    cout << N << endl;
+    array<vector<int>, 67> indexes_of_remains;
+    for (int i(0); i < N; i++){
+        int num(0);
+        cin >> num;
+        indexes_of_remains[num%67].push_back(i);
+    }
+    for (int i(0); i < 67; i++){
+        if (indexes_of_remains[i].size()>0){
+            cout << i << ": ";
+            for (int j(0); j < indexes_of_remains[i].size(); j++) {
+                cout << indexes_of_remains[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
 }
 
 
