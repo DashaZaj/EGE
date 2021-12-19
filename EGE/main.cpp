d78a67d149ea38cbcdfaf846fbaa1690e2ed9c2a
Dasha_Zajc <no@no.no>
2021-12-19 18:54:21 +0300

    vector created

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -7,13 +7,20 @@ using namespace std;
 void num_sum_div_67_distance_not_closer_than_5(){
     ifstream ifs("data.txt");
     /*
-    1. записываем числа в массив
+    +1. записываем числа в массив
     2. проверяем и считаем все возможные с учётом условия пары на кратность 67-ми
     **/
     int N(0);
     ifs >> N;
-    cout << N << endl;
+    vector <int> numbers(N);
+    for (int i(0); i < N; i++){
+        ifs >> numbers[i];
+    }
+    for (int i(0); i < N; i++){
+        cout << numbers[i] << " ";
+    }
 }
+
 
 int main()
 {
