57cec68ba5d7625bbc9328f28f45048ff6f26c5c
Dasha_Zajc <no@no.no>
2021-12-19 19:39:42 +0300

    added development plan; can read from file

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
+void num_sum_div_67_distance_not_closer_than_5(){
+    ifstream cin("data.txt");
+    /*
+    1. создать массив векторв, содержащих номера чисел, соответственно остатку от деления на 67
+    2. найти пары векторов, остатки которых комплиминтарны
+    3. для каждой пары остатков найти количество пар, отвечающих условию о расстоянии
+    **/
+    int N(0);
+    cin >> N;
+    cout << N << endl;
+}
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    num_sum_div_67_distance_not_closer_than_5();
     return 0;
 }
