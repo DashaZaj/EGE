0c705e44bb2ee5c528e405cbaf9b8b821ae02b6a
Dasha_Zajc <no@no.no>
2021-12-19 18:51:15 +0300

    added development plan; can read from file

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
+void num_sum_div_67_distance_not_closer_than_5(){
+    ifstream ifs("data.txt");
+    /*
+    1. записываем числа в массив
+    2. проверяем и считаем все возможные с учётом условия пары на кратность 67-ми
+    **/
+    int N(0);
+    ifs >> N;
+    cout << N << endl;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    num_sum_div_67_distance_not_closer_than_5();
     return 0;
 }
