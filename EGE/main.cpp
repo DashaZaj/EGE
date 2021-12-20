e3c7b88ad390eb979defa9080fb469f88e1ae0a8
Dasha_Zajc <no@no.no>
2021-12-20 16:34:53 +0300

    added development plan; can read from file

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+void num_div_sum_multiple_distance(){
+    ifstream cin("data.txt");
+    /*
+    1. записать числа в массив
+    2. запустить вложенный цикл по всем возможным парам и посчитать подходящие условию
+    **/
+    cout << "Hello world!" << endl;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    num_div_sum_multiple_distance();
     return 0;
 }
