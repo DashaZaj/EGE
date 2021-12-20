b7f2944fbb048ac0c94d38ab13f6c3c2a8bae50a
Dasha_Zajc <no@no.no>
2021-12-20 19:59:10 +0300

    "worm" became "snake" but nothing really changed

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -28,17 +28,15 @@ void min_sum_distance()
     for (int i(0); i<4; i++){
         ifs >> worm[i];
     }
-
+    int head(3), tail(0);
     for (int j(0); j < n-4; j++){
-        if (min1 > worm[0]) min1 = worm[0];
-        int temp(0);
-        for (int i(3); i>=0; i=i-1){
-            temp = temp + worm[i];
-            worm[i] = temp - worm[i];
-            temp = temp - worm[i];
-        }
-        ifs >> worm[3];
-        if (par > (min1 + worm[3])) par = min1 + worm[3];
+        if (min1 > worm[tail]) min1 = worm[tail];
+        if (head <3) head++;
+        else head = 0;
+        if (tail < 3) tail++;
+        else tail = 0;
+        ifs >> worm[head];
+        if (par > (min1 + worm[head])) par = min1 + worm[head];
     }
     cout << par << endl;
 }
