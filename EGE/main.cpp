a86581f0c2585b2bf42dacdeaada80b32f53d503
Dasha_Zajc <no@no.no>
2021-12-20 19:21:11 +0300

    buffer created

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -8,13 +8,18 @@ using namespace std;
 void num_sum_div_67_distance_not_closer_than_5() {
     ifstream cin("data.txt");
     /*
-    1. создать циклический буфер длиной 5
+    +1. создать циклический буфер длиной 5
     2. заполнять массив счётчиков остатков от деления на 67 по мере продвижения буфера
     3. прибавлять к ответу значение счётчика чисел с остатком, комплиментарным числу в head буфера; вывести ответ в конце процесса
     **/
     int N(0);
     cin >> N;
-    cout << N << endl;
+    int teil(0), head(4);
+    vector <int> buffer(5);
+    for (int i(0); i<5; i++)
+        cin >> buffer[i];
+    for (int i(0); i<5; i++)
+        cout << buffer[i] << endl;
 }
 
 
