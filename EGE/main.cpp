eda0bd49a2ad4de7dc4f49b76d7a575da5667e27
Dasha_Zajc <no@no.no>
2021-12-20 17:38:06 +0300

    three vectors are created

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <array>
 #include <fstream>
 
 using namespace std;
@@ -6,11 +8,20 @@ using namespace std;
 void num_div_sum_multiple_distance(){
     ifstream cin("data.txt");
     /*
-    1. создать три среза и записать туда числа
+    +1. создать три среза и записать туда числа
     2. сохранять в массивы не сами числа, а остатки от деления их на 36
     3. запустить три цикла на подсчёт комлиментарных пар и вывести результат
     **/
-    cout << "Hello world!" << endl;
+    int N(0), num(0);
+    cin >> N;
+    array<vector<int>, 3> slice;
+    for (int i(0); i<N; i++){
+        cin >> num;
+        slice[i%3].push_back(num);
+    }
+    for (int j(0); j<3; j++){
+        cout << slice[j].size() << endl;
+    }
 }
 
 int main()
