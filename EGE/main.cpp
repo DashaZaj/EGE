ec867d5258ad23ea9eac0a0f399a2ccabf3c95ef
Dasha_Zajc <no@no.no>
2021-12-20 17:50:00 +0300

    arrays of counters of remains are created

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -9,18 +9,23 @@ void num_div_sum_multiple_distance(){
     ifstream cin("data.txt");
     /*
     +1. создать три среза и записать туда числа
-    2. сохранять в массивы не сами числа, а остатки от деления их на 36
+    +2. сохранять в массивы не сами числа, а остатки от деления их на 36
     3. запустить три цикла на подсчёт комлиментарных пар и вывести результат
     **/
     int N(0), num(0);
     cin >> N;
-    array<vector<int>, 3> slice;
+    array<array<int, 36>, 3> slice;
+    for (int h(0); h < 3; h++)
+        for (int l(0); l < 36; l++)
+            slice[h][l] = 0;
     for (int i(0); i<N; i++){
         cin >> num;
-        slice[i%3].push_back(num);
+        slice[i%3][num%36]++;
     }
     for (int j(0); j<3; j++){
-        cout << slice[j].size() << endl;
+        for (int i(0); i < 36; i++)
+            cout << slice[j][i] << " ";
+        cout << endl;
     }
 }
 
