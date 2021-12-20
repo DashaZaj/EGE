3cfd934f1c2f3dc1f9ab1c2cf028522dacd4fc41
Dasha_Zajc <no@no.no>
2021-12-20 16:49:36 +0300

    naive solution is found

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -8,15 +8,19 @@ void num_div_sum_multiple_distance(){
     ifstream cin("data.txt");
     /*
     +1. записать числа в массив
-    2. запустить вложенный цикл по всем возможным парам и посчитать подходящие условию
+    +2. запустить вложенный цикл по всем возможным парам и посчитать подходящие условию
     **/
-    int N(0);
+    int N(0), counter(0);
     cin >> N;
     vector <int> numbers(N);
     for (int i(0); i < N; i++)
         cin >> numbers[i];
-    for (int i(0); i < N; i++)
-        cout << numbers[i] << " ";
+    for (int i(0); i < N-1; i++){
+        for (int j(i+1); j<N; j++){
+            if ((numbers[i] + numbers[j])%36==0 and (j-i)%3==0) counter++;
+        }
+    }
+    cout << counter << endl;
 }
 
 int main()
