90b14b879ebdc4651993503dab312415c825997f
Dasha_Zajc <no@no.no>
2021-12-20 17:27:23 +0300

    added development plan; can read from file

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+void num_div_sum_multiple_distance(){
+    ifstream cin("data.txt");
+    /*
+    1. создать три среза и записать туда числа
+    2. сохранять в массивы не сами числа, а остатки от деления их на 36
+    3. запустить три цикла на подсчёт комлиментарных пар и вывести результат
+    **/
+    cout << "Hello world!" << endl;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    num_div_sum_multiple_distance();
     return 0;
 }
