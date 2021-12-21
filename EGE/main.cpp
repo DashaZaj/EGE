55bdaa1acc126679f9eb064cbd689158093b7d83
Zajceva Darya <no@no.no>
2021-12-21 11:54:04 +0300

    in-process debugging

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -8,17 +8,18 @@ void num_three_prod_div_4(){
     ifstream cin("data.txt");
     /*
     +1. ввести данные
-    +2. в процессе считывания данных считать количество чисел, кратных 4(div)
-    +3. вывести ответ(div*(n-1)*(n-2))
+    +2. в процессе считывания данных считать количество чисел, кратных 4(div4) и числа, кратные 2(div2)
+    +3. вывести ответ
     **/
-    int n(0), div(0);
+    int n(0), div4(0), div2(0);
     cin >> n;
     int cur(0);
     for (int i(0); i< n; i++){
         cin >> cur;
-        if (cur % 4== 0) div++;
+        if (cur % 4== 0) div4++;
+        else if (cur %2 == 0) div2++;
     }
-    cout << div*(n-1)*(n-2) << endl;
+    cout << div4*(n-1)*(n-div2-2) + div2*(div2 - 1)*(n-2-div4) << endl;
 }
 
 int main()
