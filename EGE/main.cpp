b42690fde8d6985a62be8749eebac830770a638d
Zajceva Darya <no@no.no>
2021-12-21 10:45:32 +0300

    vector of numbers created

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <fstream>
 
 using namespace std;
@@ -7,7 +8,7 @@ void num_three_prod_div_4(){
     ifstream cin("data.txt");
     /*
     вычисляется количество троек различных элементов, произведение которых делится на 4.
-    1. считвть числа в вектор
+    +1. считать числа в вектор
     2. вывести все возможные пары чисел без учёта порядка
     3. вывести все тройки чисел без учёта порядка
     4. вывести только тройки, произведение членов которой кратно 4
@@ -15,7 +16,11 @@ void num_three_prod_div_4(){
     **/
     int N(0);
     cin >> N;
-    cout << N << endl;
+    vector <int> numbers(N, 0);
+    for (int i(0); i < N; i++)
+        cin >> numbers[i];
+    for (int i(0); i < N; i++)
+        cout << numbers[i] << " ";
 }
 
 int main()
