6d1f882c03daee2be1dc79e6220dafb839820a08
Zajceva Darya <no@no.no>
2021-12-21 11:26:53 +0300

    multiples of 4 are counted

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -8,16 +8,17 @@ void num_three_prod_div_4(){
     ifstream cin("data.txt");
     /*
     +1. ввести данные
-    2. в процессе считывания данных считать количество чисел, чратных 4(div)
+    +2. в процессе считывания данных считать количество чисел, кратных 4(div)
     3. вывести ответ(div*(n-1)*(n-2))
     **/
-    int n(0);
+    int n(0), div(0);
     cin >> n;
-    vector <int> numbers(n,0);
-    for (int i(0); i< n; i++)
-        cin >> numbers[i];
-    for (int i(0); i< n; i++)
-        cout << numbers[i] << " ";
+    int cur(0);
+    for (int i(0); i< n; i++){
+        cin >> cur;
+        if (cur % 4== 0) div++;
+    }
+    cout << div << endl;
 }
 
 int main()
