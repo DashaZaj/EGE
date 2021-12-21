e0e9a7a7532ef0417d7f8ea5872d00c851b6ad6c
Zajceva Darya <no@no.no>
2021-12-21 11:23:35 +0300

    data can be read

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <fstream>
 
 using namespace std;
@@ -6,13 +7,17 @@ using namespace std;
 void num_three_prod_div_4(){
     ifstream cin("data.txt");
     /*
-    1. ввести данные
+    +1. ввести данные
     2. в процессе считывания данных считать количество чисел, чратных 4(div)
     3. вывести ответ(div*(n-1)*(n-2))
     **/
     int n(0);
     cin >> n;
-    cout << n << endl;
+    vector <int> numbers(n,0);
+    for (int i(0); i< n; i++)
+        cin >> numbers[i];
+    for (int i(0); i< n; i++)
+        cout << numbers[i] << " ";
 }
 
 int main()
