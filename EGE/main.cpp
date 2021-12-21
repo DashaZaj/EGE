4ab8848daa3e393f6f88eb0da1ea84d99271facb
Zajceva Darya <no@no.no>
2021-12-21 11:19:28 +0300

    added development plan;can read from file

--- EGE/main.cpp
+++ EGE/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+void num_three_prod_div_4(){
+    ifstream cin("data.txt");
+    /*
+    1. ввести данные
+    2. в процессе считывания данных считать количество чисел, чратных 4(div)
+    3. вывести ответ(div*(n-1)*(n-2))
+    **/
+    int n(0);
+    cin >> n;
+    cout << n << endl;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    num_three_prod_div_4();
     return 0;
 }
